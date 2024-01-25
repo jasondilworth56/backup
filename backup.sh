8da5f9aa09aa4a1e9b833d84466ade6fc696a4de
Jason Dilworth <hello@jasondilworth.co.uk>
2024-01-25 10:00:22 +0000

    Remove externals

--- backup.sh
+++ backup.sh
@@ -49,7 +49,7 @@ source "${CONFIG}"
 ### CHECKS ###
 
 # This section checks for all of the binaries used in the backup
-BINARIES=( cat cd command date dirname echo find openssl pwd realpath rm rsync scp ssh tar )
+BINARIES=( cat cd command date dirname echo find pwd realpath rm tar )
 
 # Iterate over the list of binaries, and if one isn't found, abort
 for BINARY in "${BINARIES[@]}"; do
@@ -70,20 +70,6 @@ elif [ ! -w "${TEMPDIR}" ]; then
     exit
 fi
 
-# Check that SSH login to remote server is successful
-if [ ! "$(ssh -oBatchMode=yes -p "${REMOTEPORT}" "${REMOTEUSER}"@"${REMOTESERVER}" echo test)" ]; then
-    log "Failed to login to ${REMOTEUSER}@${REMOTESERVER}"
-    log "Make sure that your public key is in their authorized_keys"
-    exit
-fi
-
-# Check that remote directory exists and is writeable
-if ! ssh -p "${REMOTEPORT}" "${REMOTEUSER}"@"${REMOTESERVER}" test -w "${REMOTEDIR}" ; then
-    log "Failed to write to ${REMOTEDIR} on ${REMOTESERVER}"
-    log "Check file permissions and that ${REMOTEDIR} is correct"
-    exit
-fi
-
 BACKUPDATE=$(date -u +%Y-%m-%d-%H%M)
 STARTTIME=$(date +%s)
 TARFILE="${LOCALDIR}""$(hostname)"-"${BACKUPDATE}".tgz
@@ -92,23 +78,6 @@ SQLFILE="${TEMPDIR}mysql_${BACKUPDATE}.sql"
 cd "${LOCALDIR}" || exit
 
 ### END OF CHECKS ###
-
-### MYSQL BACKUP ###
-
-if [ ! "$(command -v mysqldump)" ]; then
-    log "mysqldump not found, not backing up MySQL!"
-elif [ -z "$ROOTMYSQL" ]; then
-    log "MySQL root password not set, not backing up MySQL!"
-else
-    log "Starting MySQL dump dated ${BACKUPDATE}"
-    mysqldump -u root -p"${ROOTMYSQL}" --all-databases > "${SQLFILE}"
-    log "MySQL dump complete"; log ""
-
-    #Add MySQL backup to BACKUP list
-    BACKUP=(${BACKUP[*]} ${SQLFILE})
-fi
-
-### END OF MYSQL BACKUP ###
 
 ### TAR BACKUP ###
 
@@ -138,44 +107,12 @@ rm "${TARFILE}"
 BACKUPSIZE=$(du -h "${TARFILE}".enc | cut -f1)
 log "Tar backup complete. Filesize: ${BACKUPSIZE}"; log ""
 
-# Transfer to remote server
-log "Tranferring tar backup to remote server"
-
-# Check if bandwidth limiting is enabled
-if [ "${SCPLIMIT}" -gt 0 ]; then 
-    scp -l "${SCPLIMIT}" -P "${REMOTEPORT}" "${TARFILE}".enc "${REMOTEUSER}"@"${REMOTESERVER}":"${REMOTEDIR}"
-else
-    scp -P "${REMOTEPORT}" "${TARFILE}".enc "${REMOTEUSER}"@"${REMOTESERVER}":"${REMOTEDIR}"
-fi
-log "File transfer completed"; log ""
-
-if [ "$(command -v mysqldump)" ]; then
-    if [ ! -z "${ROOTMYSQL}" ]; then
-        log "Deleting temporary MySQL backup"; log ""
-        rm "${SQLFILE}"
-    fi
-fi
-
 ### END OF TAR BACKUP ###
-
-### RSYNC BACKUP ###
-
-log "Starting rsync backups"
-for i in "${RSYNCDIR[@]}"; do
-    rsync -aqz --no-links --progress --delete --relative -e"ssh -p ${REMOTEPORT}" "$i" "${REMOTEUSER}"@"${REMOTESERVER}":"${REMOTEDIR}"
-done
-log "rsync backups complete"; log ""
-
-### END OF RSYNC BACKUP ###
 
 ### BACKUP DELETION ##
 
 log "Checking for LOCAL backups to delete..."
 bash "${SCRIPTDIR}"/deleteoldbackups.sh --config "${CONFIG}"
-log ""
-
-log "Checking for REMOTE backups to delete..."
-bash "${SCRIPTDIR}"/deleteoldbackups.sh --config "${CONFIG}" --remote
 log ""
 
 ### END OF BACKUP DELETION ###
